99e79a76344e189b76b319a536e1b8a244c35259
Ravi Shanker Kushwaha <ravishankerk48@gmail.com>
2022-06-05 21:44:43 +0530

    don't know what changes made

--- personalfolio/settings.py
+++ personalfolio/settings.py
@@ -80,22 +80,13 @@ WSGI_APPLICATION = 'personalfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-import dj_database_url
-
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'rsk',
-        'PASSWORD': 'adminrsk',
-        'HOST': 'localhost',
-        'PORT': '',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 
 
 # Password validation
